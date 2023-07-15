797c02ee47939636395fd50d8cb6f26e363970bf
Daniela <Daniela@DESKTOP-P4BHM2A>
2023-07-15 16:01:52 +0100

    Adicionar arquivos de projeto.

---
+++ Exercise3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Daniela";
+            string sobrenome = "Duarte";
+
+            Console.WriteLine(nome + " " + sobrenome);
+            Console.ReadLine();
+        }
+    }
+}
